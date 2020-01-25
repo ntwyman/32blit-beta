0f92a95311feb2e9604037093e0ae5870469e517
Nick Twyman <nick@truss.works>
2020-01-24 19:03:40 -0800

    Rendering everything now. Also tidied up Sprite data files

---
+++ examples/chuckie-egg/sprites.hpp
@@ -0,0 +1,31 @@
+#include "types/rect.hpp"
+
+using blit::rect;
+typedef rect Sprite;
+extern const Sprite &SpriteEgg;
+extern const Sprite &SpriteWall;
+extern const Sprite &SpriteLadder;
+extern const Sprite &SpriteGrain;
+extern const Sprite &SpriteScore;
+extern const Sprite &SpriteBlank;
+extern const Sprite &SpritePlayer;
+extern const Sprite &SpriteBonus;
+extern const Sprite &SpriteLevel;
+extern const Sprite &SpriteTime;
+extern const Sprite &SpriteCage;
+extern const Sprite &SpriteCageOpen;
+extern const Sprite &SpriteDuck;
+extern const Sprite &SpriteDuckStep;
+extern const Sprite &SpriteDuckUp;
+extern const Sprite &SpriteDuckUpStep;
+extern const Sprite &SpriteDuckEat;
+extern const Sprite &SpriteDuckEat2;
+extern const Sprite &SpriteLift;
+extern const Sprite &SpriteBigDuck;
+extern const Sprite &SpriteBigDuckFrame;
+
+extern Sprite SpriteDigits[];
+extern Sprite SpriteHenryWalks[];
+extern Sprite SpriteHenryClimbs[];
+
+extern uint8_t *getSpriteData();
